8d170aae6f8f6ba699edbf7dfcc5776a8328dcda
sniper1rdr <47666879+sniper1rdr@users.noreply.github.com>
2026-09-05 14:23:16 +0300

    Make EnvironmentRoot editor-safe: use Undo when creating/destroying environment objects

--- TileWorldCreator/Script/Core/EnvironmentRoot.cs
+++ TileWorldCreator/Script/Core/EnvironmentRoot.cs
@@ -1,6 +1,10 @@
 using UnityEngine;
 using System.Collections.Generic;
 
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
 namespace TileWorldCreator
 {
     [AddComponentMenu("TileWorld/Core/Environment Root")]
@@ -14,14 +18,21 @@ namespace TileWorldCreator
         public GameObject AddEnvironmentObject(GameObject prefab, Vector3 position)
         {
             if (prefab == null) return null;
-            
+
+#if UNITY_EDITOR
+            GameObject obj = PrefabUtility.InstantiatePrefab(prefab) as GameObject;
+            if (obj == null)
+                obj = Instantiate(prefab);
+            Undo.RegisterCreatedObjectUndo(obj, "Add Environment Object");
+#else
             GameObject obj = Instantiate(prefab);
+#endif
             obj.transform.position = position;
             obj.transform.SetParent(transform, true);
             obj.name = $"{prefab.name}_{environmentObjects.Count}";
-            
+
             environmentObjects.Add(obj);
-            
+
             return obj;
         }
 
@@ -30,17 +41,30 @@ namespace TileWorldCreator
             if (environmentObjects.Contains(obj))
             {
                 environmentObjects.Remove(obj);
+#if UNITY_EDITOR
+                Undo.DestroyObjectImmediate(obj);
+#else
                 DestroyImmediate(obj);
+#endif
             }
         }
 
         public void ClearEnvironment()
         {
+#if UNITY_EDITOR
+            for (int i = environmentObjects.Count - 1; i >= 0; i--)
+            {
+                GameObject obj = environmentObjects[i];
+                if (obj != null)
+                    Undo.DestroyObjectImmediate(obj);
+            }
+#else
             foreach (GameObject obj in environmentObjects)
             {
                 if (obj != null)
                     DestroyImmediate(obj);
             }
+#endif
             environmentObjects.Clear();
         }
     }
